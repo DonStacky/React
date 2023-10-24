71429a564832e831ef0b0f85027481a92871be87
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-10-24 15:39:09 +0300

    fix: husky pre-commit

--- react-components/src/components/search-field/search-field.tsx
+++ react-components/src/components/search-field/search-field.tsx
@@ -2,16 +2,20 @@ import { Component } from 'react';
 import './search-field.scss';
 
 export class SearchField extends Component {
+
   render() {
     return (
-      <form className="search-field__form container">
+          <form className="search-field__form container">
         <input
           type="text"
-          className="search-field__input"
-          placeholder="Enter to search"
+          className="search-field__input" 
+          placeholder="Enter to search" 
+
         />
         <button className="search-field__button">Search</button>
+        
       </form>
+
     );
   }
 }
