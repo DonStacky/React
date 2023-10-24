f8192e05cd279acd9dcbb5eaa152887bd895c9c6
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-10-24 11:37:48 +0300

    feat: add header

---
+++ react-components/src/components/search-field/search-field.tsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import './search-field.scss';
+
+export class SearchField extends Component {
+  render() {
+    return (
+      <form className="search-field__form container">
+        <input
+          type="text"
+          className="search-field__input"
+          placeholder="Enter to search"
+        />
+        <button className="search-field__button">Search</button>
+      </form>
+    );
+  }
+}
